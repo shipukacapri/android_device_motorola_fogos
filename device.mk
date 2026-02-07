d1c3b6700dbb5fdf892315f610a6ce8bd7b538a0
Shipu <242039583+shipukacapri@users.noreply.github.com>
2026-02-07 10:16:20 +0530

    add gamebar overlay

--- device.mk
+++ device.mk
@@ -33,6 +33,9 @@ PRODUCT_PACKAGES += \
     WifiResDeviceXT2363-8 \
     WifiResDeviceXT2363-9
 
+# GameBar Performance Overlay
+$(call inherit-product, packages/apps/GameBar/gamebar.mk)
+
 # Audio
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_io_policy.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_io_policy.conf \
