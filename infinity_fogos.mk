20a30f605198311044d1c8bf1126e265f5120c94
Shipu <242039583+shipukacapri@users.noreply.github.com>
2026-05-11 11:43:18 +0530

    fogos: InfinityX Bringup (makefile)

--- infinity_fogos.mk
+++ infinity_fogos.mk
@@ -11,14 +11,17 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from fogos device
 $(call inherit-product, device/motorola/fogos/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common InfinityX stuff.
+$(call inherit-product, vendor/infinity/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_fogos
+PRODUCT_NAME := infinity_fogos
 PRODUCT_DEVICE := fogos
 PRODUCT_MANUFACTURER := motorola
 PRODUCT_BRAND := motorola
 PRODUCT_MODEL := moto g34 5G
+
+# Infinity Flags
+INFINITY_MAINTAINER := Shipu
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
 
