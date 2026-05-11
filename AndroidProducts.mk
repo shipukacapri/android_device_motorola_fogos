4d5c3b35fedd98a3c9aecdc8118fee83e029cb31
Shipu <242039583+shipukacapri@users.noreply.github.com>
2026-05-11 11:41:27 +0530

    fogos: InfinityX Bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,7 @@
-#
-# SPDX-FileCopyrightText: The LineageOS Project
-# SPDX-License-Identifier: Apache-2.0
-#
-
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_fogos.mk
+    $(LOCAL_DIR)/infinity_fogos.mk
+
+COMMON_LUNCH_CHOICES := \
+    infinity_fogos-user \
+    infinity_fogos-userdebug \
+    infinity_fogos-eng
